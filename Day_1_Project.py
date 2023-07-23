d2533e77034d9b618c669b655d1ccd6e5ccfefcd
sundydeng88 <134228073+sundydeng88@users.noreply.github.com>
2023-07-23 16:18:34 -0400

    Delete Day_1_Project.py

--- Day_1_Project.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the bandname generator!")
-city = input("What city did you grow up in?")
-pet = input("What is the name of your pet?")
-print("The name of your band is:" + city + " " + pet)
