d88e76c846fe0f40dae62f29457806001b40c439
Sundy Drng <134228073+sundydeng88@users.noreply.github.com>
2023-07-27 23:30:21 -0400

    Update Day4_Rock_Paper_Scissors.py

--- Day4_Rock_Paper_Scissors.py
+++ Day4_Rock_Paper_Scissors.py
@@ -27,34 +27,29 @@ scissors = '''
 ---.__(___)
 '''
 
-photo = [rock, paper, scissors]
 
+photo = [rock, paper, scissors]
+computer = random.randint(0,1)
 
 player = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors."))
-print(photo[player])
-
-computer = random.randint(0,1)
-print("Computer chose:")
-print(photo[computer])
-
-
-#computer = random.randint(0,2)
-#print("Computer chose:")
-#print(photo[computer])
-
-if computer == 1 and player == 0:
-  print("You lose!")
-if computer == 2 and player == 0:
-  print("You win!")
-elif computer == 0 and player == 1:
-  print("You win!")
-elif computer == 2 and player == 1:
-  print("You lose!")
-elif computer == 0 and player == 2:
-  print("You lose!")
-elif computer == 1 and player == 2:
-  print("You win!")
-elif computer == player:
-  print("Tie!")
-else:
-  print("Invalid!")
+if player >= 3 or player <0:
+    print("Invalid!")
+else: 
+    print(photo[player])
+    print("Computer chose:")
+    print(photo[computer])
+    
+    if computer == 1 and player == 0:
+        print("You lose!")
+    if computer == 2 and player == 0:
+        print("You win!")
+    elif computer == 0 and player == 1:
+        print("You win!")
+    elif computer == 2 and player == 1:
+        print("You lose!")
+    elif computer == 0 and player == 2:
+        print("You lose!")
+    elif computer == 1 and player == 2:
+         print("You win!")
+    else:
+        print("Tie!")
